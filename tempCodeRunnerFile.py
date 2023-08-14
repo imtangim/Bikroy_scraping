acc20b09dc2c4817a967cf0a6d28731c95e1e899
Tangim <tangim15-4103@diu.edu.bd>
2023-08-15 01:01:52 +0600

    bikroy khaiya disi

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+json_filename = "scraped_data.json"
+# with open(json_filename, 'w', encoding='utf-8') as jsonfile:
+#     json.dump(data_to_save, jsonfile, ensure_ascii=False, indent=4)
+
+# print(f"Data saved to {json_filename}")
